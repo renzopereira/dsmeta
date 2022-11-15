493b04bcbaa155811f9e52b83f68635285deecca
Renzo Pereira <pereirarenzodev@gmail.com>
2022-11-15 15:03:57 -0300

    Fisrt component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return(
-    <h1>Ola, Mundo!</h1>
+  return (
+    <>
+      <h1>Ola, Mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
